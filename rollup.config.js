76ebbb0dfb3b599606d2279a2eb966b35fa969c1
Michael Burger <michael.burger@orma-solutions.it>
2017-09-12 17:06:46 +0200

    first working vesion with babel to transpile to es2015

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,18 @@
-// import babel from 'rollup-plugin-babel';
-import buble from 'rollup-plugin-buble';
-import alias from 'rollup-plugin-alias';
-
-const path = require('path');
+// Rollup plugins
+import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: path.join(__dirname, 'example.js'),
-    targets: [
-        {dest: path.join(__dirname, 'example/streamedian.js'), format: 'es'}
-    ],
-    sourceMap: true,
+    name: 'html5_rtsp_player',
+    input: 'src/index.js',
+    output: {
+        file: 'build/js/player.js',
+        format: 'cjs'
+    },
+    // sourcemap: 'inline',
     plugins: [
-        //buble({
-            //exclude: 'node_modules/**'
-        //}),
-        alias({
-            bp_logger: path.join(__dirname,'node_modules/bp_logger/logger.js'),
-            bp_event: path.join(__dirname,'node_modules/bp_event/event.js'),
-            bp_statemachine: path.join(__dirname,'node_modules/bp_statemachine/statemachine.js'),
-            jsencrypt: path.join(__dirname,'node_modules/jsencrypt/src/jsencrypt.js'),
-            rtsp: path.join(__dirname,'node_modules/html5_rtsp_player/src'),
-            streamedian: path.join(__dirname,'src')
-        })
-    ]
-
-}
+        babel({
+            exclude: 'node_modules/**',  // only transpile our source code
+            include: 'node_modules/bp_**'
+        }),
+      ],
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { StreamType, WSPlayer } from './player';
+import { LogLevel, setDefaultLogLevel, getTagged } from './deps/bp_logger.js';
+
+
+export { StreamType, WSPlayer, LogLevel, setDefaultLogLevel, getTagged }
